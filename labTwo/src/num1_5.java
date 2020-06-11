4d598a0b98361fbe4c1774af5f6d0b1989ad5e66
Alinachin <alinachinfy@gmail.com>
2020-06-11 18:00:35 +0900

    Задание 2.1(пример 2.5)

---
+++ labTwo/src/num1_5.java
@@ -0,0 +1,11 @@
+public class num1_5{
+public static int f(int n){
+        if(n==0){
+        return 0;}
+        else
+        if(n==1){
+        return 1;}
+        else{
+        return f(n-2)+f(n-1);}
+        }
+        }
